b083ed47f70b5f07310417a85d5ae0d6d426082f
ashwani yadav <22ashwaniyadav@gmail.com>
2025-06-23 15:41:26 +0530

    fix: set correct base for vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: '/fontendecokaagazz/', // 👈 this is crucial for GitHub Pages
+//   plugins: [react()],
+// })
+
 export default defineConfig({
-  base: '/fontendecokaagazz/', // 👈 this is crucial for GitHub Pages
+  base: './', // ✅ correct
   plugins: [react()],
-})
+});
