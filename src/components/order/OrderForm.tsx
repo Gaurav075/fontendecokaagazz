6438451014f6f3828f8ff1e85778933c658b4b92
ashwani yadav <22ashwaniyadav@gmail.com>
2025-06-23 15:22:25 +0530

    adding razorpay

--- src/components/order/OrderForm.tsx
+++ src/components/order/OrderForm.tsx
@@ -67,7 +67,7 @@ const OrderForm: React.FC = () => {
       image: "logo.png", // Optional logo
       order_id: orderData.orderId,
       handler: async function (response: any) {
-        const verifyRes = await fetch("http://localhost:5000/api/payment/verify", {
+        const verifyRes = await fetch("https://kaagazzbackend.onrender.com/api/payment/verify", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
